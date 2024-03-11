6b6bba049de708e200963f932e48e85597cfc77a
Alison Cavalcanti <129519766+Alison-Cavalcanti@users.noreply.github.com>
2024-03-10 22:15:36 -0300

    Implementação conceito de listas python
    Foi utilizada a temática de itens de um personagem de RPG para demonstrar o uso de listas em python.

---
+++ Desafios de código SQUADIO  - Iniciante/challengerRPG_iniciante.py
@@ -0,0 +1,10 @@
+# Solicita ao usuário o nome dos 3 itens que o personagem possui
+item1 = input("Digite o nome do primeiro item: ")
+item2 = input("Digite o nome do segundo item: ")
+item3 = input("Digite o nome do terceiro item: ")
+
+# Exibe a lista de itens na tela
+print("Lista de itens:")
+print(f"- {item1}")
+print(f"- {item2}")
+print(f"- {item3}")
